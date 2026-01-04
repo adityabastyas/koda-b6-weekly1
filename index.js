4ad023c292441e4324f1647d7b877ebb8a4973ea
aditya bastyas <adityabastyasmulya@gmail.com>
2026-01-04 23:19:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { stdin } from "node:process";
 import { tanyaUser } from "./input.js";
+import { tampilkanMenu } from "./menu.js";
+import { tampilkanKeranjang, tambahKeKeranjang, hapusKeranjang } from "./keranjang.js";
 
 console.log("Selamat Datang di Pizza Hut Indonesia");
 
 const url = "https://raw.githubusercontent.com/adityabastyas/koda-b6-weekly1/refs/heads/main/data.json";
 
 let menuData = []; 
+let keranjang  = []
 
 const tampilkanMenuUtama = () => {
   console.log("== MENU UTAMA ==");
@@ -21,15 +24,35 @@ const tampilkanMenuUtama = () => {
 const menuUtama = async () => {
   tampilkanMenuUtama();
   
-  const pilihan = await tanyaUser("pilih menu: ");
+  const pilihan = await tanyaUser("pilih nomor menu: ");
   
   if (pilihan === "1") {
     tampilkanMenu(menuData);
     await menuUtama(); 
-    
+  } else if (pilihan === "2") {
+    await tambahKeKeranjang(menuData, keranjang, tampilkanMenu)
+    await menuUtama(); 
+  } else if (pilihan === "3") {
+    tampilkanKeranjang(keranjang)
+    await menuUtama(); 
+
+  } else if (pilihan === "4") {
+    await hapusKeranjang(keranjang, tambahKeKeranjang)
+    await menuUtama();
+  
+  } else if (pilihan === "5") {
+    await menuUtama();
+
+
+  } else if (pilihan === "6") {
+    await menuUtama();
+
+
+  } else if(pilihan === 7) {
+    console.log("terimakasih")
+
     stdin.pause();
     process.exit(0);
-    
   } else {
     console.log("pilihan tidak ada ");
     await menuUtama(); 
