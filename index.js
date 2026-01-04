c66ac327a97c9656048b2206499c6f37c6df4fa7
aditya bastyas <adityabastyasmulya@gmail.com>
2026-01-04 09:39:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-import { createInterface } from "node:readline/promises";
-import { stdin, stdout } from "node:process";
-const rl = createInterface({
-  input: stdin,
-  output: stdout,
-});
+import { stdin } from "node:process";
+import { tanyaUser } from "./input.js";
+
 console.log("Selamat Datang di Pizza Hut Indonesia");
 
-const url =
-  "https://raw.githubusercontent.com/adityabastyas/koda-b6-weekly1/refs/heads/main/data.json";
+const url = "https://raw.githubusercontent.com/adityabastyas/koda-b6-weekly1/refs/heads/main/data.json";
+
+let menuData = []; 
+
+const tampilkanMenuUtama = () => {
+  console.log("== MENU UTAMA ==");
+  console.log("1. menu pizza");
+  console.log("2. tambah ke keranjang");
+  console.log("3. lihat keranjang");
+  console.log("4. hapus item keranjang");
+  console.log("5. checkout");
+  console.log("6. lihat histori");
+  console.log("7. keluar");
+};
+
+const menuUtama = async () => {
+  tampilkanMenuUtama();
+  
+  const pilihan = await tanyaUser("pilih menu: ");
+  
+  if (pilihan === "1") {
+    tampilkanMenu(menuData);
+    await menuUtama(); 
+    
+    stdin.pause();
+    process.exit(0);
+    
+  } else {
+    console.log("pilihan tidak ada ");
+    await menuUtama(); 
+  }
+};
 
 const pizzaHutIndonesia = async () => {
   try {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    const target = data.map((item) => {
-      let menu = item.name;
-      // let harga = item.harga;
-
-      return menu;
-    });
-
-    console.log(target);
-    const pilih = await rl.question("Silahkan pilih menu pizza): ");
-    console.log(`pesanan ${pilih} berhasil`);
-
-    rl.close();
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    menuData = data;
+    
+    await menuUtama();
+    
   } catch (error) {
-    console.log(error);
+    console.log("error :", error);
+    stdin.pause();
+    process.exit(1);
   }
 };
 
